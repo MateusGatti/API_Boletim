7d28f141173c2578d8500acc697fdab448f37f65
Mateus Mello <mt.gatti@hotmail.com>
2020-09-01 16:20:32 -0300

    Aula 2 finalizada

---
+++ API_Boletim/Interfaces/IAluno.cs
@@ -0,0 +1,19 @@
+﻿using API_Boletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno aluno);
+        List<Aluno> LerAlunos();
+        Aluno BuscarPorId(int id);
+        Aluno Alterar(Aluno aluno);
+        Aluno Excluir(Aluno aluno);
+
+
+    }
+}
---
+++ API_Boletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Alterar(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerAlunos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
